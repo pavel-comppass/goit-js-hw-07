815a8998f4f008eae2034448f99d0ad4ae0fe728
pavel-comppass <83498884+pavel-comppass@users.noreply.github.com>
2021-09-09 09:28:48 +0300

    task2 and 3

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,15 @@ const ingredients = [
   'Помидоры',
   'Зелень',
   'Приправы',
-];
+]
+const ingredientsRef = document.querySelector('#ingredients')
+
+// //Mapped
+const addIngredient = (array) =>
+  array.map((element) => {
+    const itemOfListEl = document.createElement('li')
+    itemOfListEl.textContent = element
+    return itemOfListEl
+  })
+
+ingredientsRef.append(...addIngredient(ingredients))
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,31 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-];
+]
+const gallery = document.querySelector('#gallery')
+const createElements = (elements) => {
+  return elements
+    .map(({ url, alt }) => {
+      return `<li><img src="${url}" alt="${alt}" width="300px" heigth="200px"><li>`
+    })
+    .join('')
+}
+gallery.insertAdjacentHTML('afterbegin', createElements(images))
+gallery.style.marginTop = '20px'
+gallery.style.display = 'flex'
+gallery.style.justifyContent = 'space-arround'
+gallery.style.padding = '0'
+gallery.style.listStyle = 'none'
